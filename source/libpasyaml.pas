f403a1256c697450f24e1ae01e88365121a1f408
isemenkov <ivan@semenkov.pro>
2020-01-18 11:00:26 +0200

    Update bindings

--- source/libpasyaml.pas
+++ source/libpasyaml.pas
@@ -38,6 +38,38 @@ uses
 {$ENDIF}
 
 const
+  { The tag @c !!null with the only possible value: @c null. }
+  YAML_NULL_TAG = 'tag:yaml.org,2002:null';
+
+  { The tag @c !!bool with the values: @c true and @c falce. }
+  YAML_BOOL_TAG = 'tag:yaml.org,2002:bool';
+
+  { The tag @c !!str for string values. }
+  YAML_STR_TAG = 'tag:yaml.org,2002:str';
+
+  { The tag @c !!int for integer values. }
+  YAML_INT_TAG = 'tag:yaml.org,2002:int';
+
+  { The tag @c !!float for float values. }
+  YAML_FLOAT_TAG = 'tag:yaml.org,2002:float';
+
+  { The tag @c !!timestamp for date and time values. }
+  YAML_TIMESTAMP_TAG = 'tag:yaml.org,2002:timestamp';
+
+  { The tag @c !!seq is used to denote sequences. }
+  YAML_SEQ_TAG = 'tag:yaml.org,2002:seq';
+
+  { The tag @c !!map is used to denote mapping. }
+  YAML_MAP_TAG = 'tag:yaml.org,2002:map';
+
+  { The default scalar tag is @c !!str. }
+  YAML_DEFAULT_SCALAR_TAG = YAML_STR_TAG;
+
+  { The default sequence tag is @c !!seq. }
+  YAML_DEFAULT_SEQUENCE_TAG = YAML_SEQ_TAG;
+
+  { The default mapping tag is @c !!map. }
+  YAML_DEFAULT_MAPPING_TAG = YAML_MAP_TAG;
 
 type
   { The character type (UTF-8 octet). }
@@ -314,6 +346,185 @@ type
   pyaml_event_t = ^yaml_event_t;
   yaml_event_t = yaml_event_s;
 
+  { Node types. }
+  yaml_node_type_e = (
+    YAML_NO_NODE,                    { An empty node. }
+    YAML_SCALAR_NODE,                { A scalar node. }
+    YAML_SEQUENCE_NODE,              { A sequence node. }
+    YAML_MAPPING_NODE                { A mapping node. }
+  );
+  yaml_node_type_t = yaml_node_type_e;
+
+  { An element of a sequence node. }
+  yaml_node_item_t = Integer;
+
+  { An element of a mapping node. }
+  yaml_node_pair_s = record
+    key : Integer;                   { The key of the element. }
+    value : Integer;                 { The value of the element. }
+  end;
+  yaml_node_pair_t = yaml_node_pair_s;
+
+  { The node structure. }
+  pyaml_node_item_t = ^yaml_node_item_t;
+  yaml_node_s = record
+    node_type : yaml_node_type_t;    { The node type. }
+    tag : pyaml_char_t;              { The node tag. }
+    { The node data. }
+    case data : Integer of
+    { The scalar parameters (for @c YAML_SCALAR_NODE). }
+    1 : (scalar = record
+           { The scalar value. }
+           value : pyaml_char_t;
+           { The length of the scalar value. }
+           length : QWord;
+           { The scalar style. }
+           style : yaml_scalar_style_t;
+         end;
+        );
+    { The sequence parameters (for @c YAML_SEQUENCE_NODE). }
+    2 : (sequence = record
+           { The stack of sequence items. }
+           items = record
+             { The beginning of the stack. }
+             start_item : pyaml_node_item_t;
+             { The end of the stack. }
+             end_item : pyaml_node_item_t;
+             { The top of the stack. }
+             top_item : pyaml_node_item_t;
+           end;
+           { The sequence style. }
+           style : yaml_sequence_style_t;
+         end;
+        );
+    { The mapping parameters (for @c YAML_MAPPING_NODE). }
+    3 : (mapping : record
+           { The stack of mapping pairs (key, value). }
+           pairs = record
+             { The beginning of the stack. }
+             start_stack : pyaml_node_pair_t;
+             { The end of the stack. }
+             end_stack : pyaml_node_pair_t;
+             { The top of the stack. }
+             top_stack : pyaml_node_pair_t;
+           end;
+           { The mapping style. }
+           style : yaml_mapping_style_t;
+         end;
+        );
+    start_mark : yaml_mark_t;        { The beginning of the node. }
+    end_mark : yaml_mark_t;          { The end of the node. }
+  end;
+  yaml_node_item_t = yaml_node_s;
+
+  { The document structure. }
+  yaml_document_s = record
+    { The document nodes. }
+    nodes = record
+      { The beginning of the stack. }
+      start_stack : pyaml_node_t;
+      { The end of the stack. }
+      end_stack : pyaml_node_t;
+      { The top of the stack. }
+      top_stack : pyaml_node_t;
+    end;
+    version_directive : pyaml_version_directive_t; { The version directive. }
+    { The list of tag directives. }
+    tag_directives = record
+      { The beginning of the tag directives list. }
+      start_list : pyaml_tag_directive_t;
+      { The end of the tag directives list. }
+      end_list : pyaml_tag_directive_t;
+    end;
+    start_implicit : Integer;      { Is the document start indicator implicit? }
+    end_implicit : Integer;          { Is the document end indicator implicit? }
+    start_mark : yaml_mark_t;        { The beginning of the document. }
+    end_mark : yaml_mark_t;          { The end of the document. }
+  end;
+  yaml_document_t = yaml_document_s;
+
+  { The prototype of a read handler.
+
+    The read handler is called when the parser needs to read more bytes from the
+    source.  The handler should write not more than @a size bytes to the @a
+    buffer.  The number of written bytes should be set to the @a length variable.
+
+    @param[in,out]   data        A pointer to an application data specified by
+                                 yaml_parser_set_input().
+    @param[out]      buffer      The buffer to write the data from the source.
+    @param[in]       size        The size of the buffer.
+    @param[out]      size_read   The actual number of bytes read from the source.
+
+    @returns On success, the handler should return 1.  If the handler failed,
+    the returned value should be 0. On EOF, the handler should set the
+    @a size_read to 0 and return 1. }
+  yaml_read_handler_t = function (data : Pointer; buffer : PByte; size : QWord;
+    size_read : PQWord) : Integer;
+
+  { This structure holds information about a potential simple key. }
+  yaml_simple_key_s = record
+    possible : Integer;              { Is a simple key possible? }
+    required : Integer;              { Is a simple key required? }
+    token_number : QWord;            { The number of the token. }
+    mark : yaml_mark_t;              { The position mark. }
+  end;
+  yaml_simple_key_t = yaml_simple_key_s;
+
+  { The states of the parser. }
+  yaml_parser_state_e = (
+    { Expect STREAM-START. }
+    YAML_PARSE_STREAM_START_STATE.
+    { Expect the beginning of an implicit document. }
+    YAML_PARSE_IMPLICIT_DOCUMENT_START_STATE,
+    { Expect DOCUMENT-START. }
+    YAML_PARSE_DOCUMENT_START_STATE,
+    { Expect the content of a document. }
+    YAML_PARSE_DOCUMENT_CONTENT_STATE,
+    { Expect DOCUMENT-END. }
+    YAML_PARSE_DOCUMENT_END_STATE,
+    { Expect a block node. }
+    YAML_PARSE_BLOCK_NODE_STATE,
+    { Expect a block node or indentless sequence. }
+    YAML_PARSE_BLOCK_NODE_OR_INDENTLESS_SEQUENCE_STATE,
+    { Expect a flow node. }
+    YAML_PARSE_FLOW_NODE_STATE,
+    { Expect the first entry of a block sequence. }
+    YAML_PARSE_BLOCK_SEQUENCE_FIRST_ENTRY_STATE,
+    { Expect an entry of a block sequence. }
+    YAML_PARSE_BLOCK_SEQUENCE_ENTRY_STATE,
+    { Expect an entry of an indentless sequence. }
+    YAML_PARSE_INDENTLESS_SEQUENCE_ENTRY_STATE,
+    { Expect the first key of a block mapping. }
+    YAML_PARSE_BLOCK_MAPPING_FIRST_KEY_STATE,
+    { Expect a block mapping key. }
+    YAML_PARSE_BLOCK_MAPPING_KEY_STATE,
+    { Expect a block mapping value. }
+    YAML_PARSE_BLOCK_MAPPING_VALUE_STATE,
+    { Expect the first entry of a flow sequence. }
+    YAML_PARSE_FLOW_SEQUENCE_FIRST_ENTRY_STATE,
+    { Expect an entry of a flow sequence. }
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_STATE,
+    { Expect a key of an ordered mapping. }
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_KEY_STATE,
+    { Expect a value of an ordered mapping. }
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_VALUE_STATE,
+    { Expect the and of an ordered mapping entry. }
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_END_STATE,
+    { Expect the first key of a flow mapping. }
+    YAML_PARSE_FLOW_MAPPING_FIRST_KEY_STATE,
+    { Expect a key of a flow mapping. }
+    YAML_PARSE_FLOW_MAPPING_KEY_STATE,
+    { Expect a value of a flow mapping. }
+    YAML_PARSE_FLOW_MAPPING_VALUE_STATE,
+    { Expect an empty value of a flow mapping. }
+    YAML_PARSE_FLOW_MAPPING_EMPTY_VALUE_STATE,
+    { Expect nothing. }
+    YAML_PARSE_END_STATE
+  );
+  yaml_parser_state_t = yaml_parser_state_e;
+
+
+
 {$IFDEF WINDOWS}
   const libYaml = 'libyaml.dll';
 {$ENDIF}
@@ -478,6 +689,128 @@ function yaml_mapping_end_event_initialize (event : pyaml_event_t) : Integer;
 
   @param[in,out]   event   An event object. }
 procedure yaml_event_delete (event : pyaml_event_t); cdecl; external libYaml;
+
+{ Create a YAML document.
+
+  @param[out]      document                An empty document object.
+  @param[in]       version_directive       The %YAML directive value or
+                                           @c NULL.
+  @param[in]       tag_directives_start    The beginning of the %TAG
+                                           directives list.
+  @param[in]       tag_directives_end      The end of the %TAG directives
+                                           list.
+  @param[in]       start_implicit          If the document start indicator is
+                                           implicit.
+  @param[in]       end_implicit            If the document end indicator is
+                                           implicit.
+
+  @returns 1 if the function succeeded, 0 on error. }
+function yaml_document_initialize (document : pyaml_document_t;
+  version_directive : pyaml_version_directive_t; tag_directives_start :
+  pyaml_tag_directive_t; tag_directives_end : pyaml_tag_directive_t;
+  start_implicit : Integer; end_implicit : Integer) : Integer; cdecl;
+  external libYaml;
+
+{ Delete a YAML document and all its nodes.
+
+  @param[in,out]   document        A document object. }
+procedure yaml_document_delete (document : pyaml_document_t); cdecl;
+  external libYaml;
+
+{ Get a node of a YAML document.
+
+  The pointer returned by this function is valid until any of the functions
+  modifying the documents are called.
+
+  @param[in]       document        A document object.
+  @param[in]       index           The node id.
+
+  @returns the node objct or @c NULL if @c node_id is out of range. }
+function yaml_document_get_node (document : pyaml_document_t; index : Integer) :
+  pyaml_node_t; cdecl; external libYaml;
+
+{ Get the root of a YAML document node.
+
+  The root object is the first object added to the document.
+
+  The pointer returned by this function is valid until any of the functions
+  modifying the documents are called.
+
+  An empty document produced by the parser signifies the end of a YAML
+  stream.
+
+  @param[in]       document        A document object.
+
+  @returns the node object or NULL if the document is empty. }
+function yaml_document_get_root_node (document : pyaml_document_t) :
+  pyaml_node_t; cdecl; external libYaml;
+
+{ Create a SCALAR node and attach it to the document.
+
+  The @a style argument may be ignored by the emitter.
+
+  @param[in,out]   document        A document object.
+  @param[in]       tag             The scalar tag.
+  @param[in]       value           The scalar value.
+  @param[in]       length          The length of the scalar value.
+  @param[in]       style           The scalar style.
+
+  @returns the node id or 0 on error. }
+function yaml_document_add_scalar (document : pyaml_document_t; tag :
+  pyaml_char_t; value : pyaml_char_t; length : Integer; style :
+  yaml_scalar_style_t) : Integer; cdecl; external libYaml;
+
+{ Create a SEQUENCE node and attach it to the document.
+
+  The @a style argument may be ignored by the emitter.
+
+  @param[in,out]   document    A document object.
+  @param[in]       tag         The sequence tag.
+  @param[in]       style       The sequence style.
+
+  @returns the node id or 0 on error. }
+function yaml_document_add_sequence (document : pyaml_document_t; tag :
+  pyaml_char_t; style : yaml_sequence_style_t) : Integer; cdecl;
+  external libYaml;
+
+{ Create a MAPPING node and attach it to the document.
+
+  The @a style argument may be ignored by the emitter.
+
+  @param[in,out]   document    A document object.
+  @param[in]       tag         The sequence tag.
+  @param[in]       style       The sequence style.
+
+  @returns the node id or 0 on error. }
+function yaml_document_add_mapping (document : pyaml_document_t; tag :
+  pyaml_char_t; style : yaml_mapping_style_t) : Integer; cdecl;
+  external libYaml;
+
+{ Add an item to a SEQUENCE node.
+
+  @param[in,out]   document    A document object.
+  @param[in]       sequence    The sequence node id.
+  @param[in]       item        The item node id.
+
+  @returns 1 if the function succeeded, 0 on error. }
+function yaml_document_append_sequence_item (document : pyaml_document_t;
+  sequence : Integer; item : Integer) : Integer; cdecl; external libYaml;
+
+{ Add a pair of a key and a value to a MAPPING node.
+
+  @param[in,out]   document    A document object.
+  @param[in]       mapping     The mapping node id.
+  @param[in]       key         The key node id.
+  @param[in]       value       The value node id.
+
+  @returns 1 if the function succeeded, 0 on error. }
+function yaml_document_append_mapping_pair (document : pyaml_document_t;
+  mapping : Integer; key : Integer; value : Integer) : Integer; cdecl;
+  external libYaml;
+
+
+
+
 
 
 implementation
